27901f92e06caa0501fb351957ef95999a030b89
Kostiantyn Polishchuk <kostya.polishchuk@gmail.com>
2026-03-19 01:00:44 +0100

    connect mongoDB

---
+++ src/models/note.js
@@ -0,0 +1,41 @@
+import { model, Schema } from 'mongoose';
+
+const noteSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: false,
+      trim: true,
+      default: '',
+    },
+    tag: {
+      type: String,
+      required: false,
+      trim: true,
+      enum: [
+        'Work',
+        'Personal',
+        'Meeting',
+        'Shopping',
+        'Ideas',
+        'Travel',
+        'Finance',
+        'Heath',
+        'Important',
+        'Todo',
+      ],
+      default: 'Todo',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -1,60 +1,33 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import pino from 'pino-http';
-import 'dotenv/config';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.json());
-
+//Global Middleware
+app.use(logger);
+app.use(express.json({ limit: '100kb' }));
 app.use(cors());
 
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat:
-          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
+//Routes
+app.use(notesRoutes);
 
-app.get('/notes', (req, res) => {
-  res.status(200).json({ message: 'Retrieved all notes' });
-});
+//404
+app.use(notFoundHandler);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  console.log(noteId);
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+//Error handler
+app.use(errorHandler);
 
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
+//MongoDB connection
+await connectMongoDB();
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
-
-app.use((err, req, res, next) => {
-  console.error('Error:', err.message);
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  res.status(500).json({
-    message: isProd ? 'Internal Server Error' : err.message,
-  });
-});
-
+//Server statup
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
