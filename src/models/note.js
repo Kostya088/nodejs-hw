2db453d9b32cfafc61314e2f4999e93e81aa6bc8
Kostiantyn Polishchuk <kostya.polishchuk@gmail.com>
2026-04-01 22:44:51 +0200

    finish tasks

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import { model, Schema } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -17,18 +18,7 @@ const noteSchema = new Schema(
       type: String,
       required: false,
       trim: true,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -37,5 +27,7 @@ const noteSchema = new Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import { errors } from 'celebrate';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -20,6 +21,10 @@ app.use(notesRoutes);
 
 //404
 app.use(notFoundHandler);
+
+// validation errors
+
+app.use(errors());
 
 //Error handler
 app.use(errorHandler);
