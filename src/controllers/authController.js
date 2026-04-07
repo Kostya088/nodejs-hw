7f3e9cb3a2cbae7bf35d1474ace6ad430c4a733a
Kostiantyn Polishchuk <kostya.polishchuk@gmail.com>
2026-04-07 21:32:26 +0200

    finish task

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,7 +1,11 @@
 import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import bcrypt from 'bcrypt';
-import { createSession, setSessionCookies } from '../services/auth.js';
+import {
+  clearSessionCookies,
+  createSession,
+  setSessionCookies,
+} from '../services/auth.js';
 import { Session } from '../models/session.js';
 
 export const registerUser = async (req, res) => {
@@ -10,7 +14,7 @@ export const registerUser = async (req, res) => {
   const existingUser = await User.findOne({ email });
 
   if (existingUser) {
-    throw createHttpError(400, 'Email in use  ');
+    throw createHttpError(400, 'Email in use');
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -48,4 +52,37 @@ export const loginUser = async (req, res) => {
   setSessionCookies(res, newSession);
 
   res.status(200).json(user);
+};
+
+export const refreshUserSession = async (req, res) => {
+  const { sessionId, refreshToken } = req.cookies;
+  const session = await Session.findOne({ _id: sessionId, refreshToken });
+
+  if (!session) {
+    throw createHttpError(401, 'Session not found');
+  }
+
+  const refreshTokenExpired =
+    new Date(session.refreshTokenValidUntil) < new Date();
+
+  if (refreshTokenExpired) {
+    throw createHttpError(401, 'Session token expired');
+  }
+
+  await Session.deleteOne({ _id: sessionId, refreshToken });
+  const newSession = await createSession(session.userId);
+
+  setSessionCookies(res, newSession);
+
+  res.status(200).json({ message: 'Session refreshed' });
+};
+
+export const logoutUser = async (req, res) => {
+  const { sessoinId } = req.cookies;
+  if (sessoinId) {
+    await Session.deleteOne({ _id: sessoinId });
+  }
+
+  clearSessionCookies(res);
+  res.status(204).send();
 };
