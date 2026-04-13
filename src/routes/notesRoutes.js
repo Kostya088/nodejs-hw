f1ff7306010e206809af5317752fac2b3c90b193
Kostiantyn Polishchuk <kostya.polishchuk@gmail.com>
2026-04-13 21:04:58 +0200

    add avatar

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -21,13 +21,9 @@ const router = Router();
 router.use('/notes', authenticate);
 
 router.get('/notes', celebrate(getAllNotesSchema), getAllNotes);
-
 router.get('/notes/:noteId', celebrate(noteIdSchema), getNoteById);
-
 router.post('/notes', celebrate(createNoteSchema), createNote);
-
 router.delete('/notes/:noteId', celebrate(noteIdSchema), deleteNote);
-
 router.patch('/notes/:noteId', celebrate(updateNoteSchema), updateNote);
 
 export default router;
